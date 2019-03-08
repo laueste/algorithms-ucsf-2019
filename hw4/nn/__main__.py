71ee2095483747a62626ffe842b87f66c6ef93f6
Laurel Estes <laurel.estes@ucsf.edu>
2019-03-07 18:20:22 -0800

    made test based off online resource with numeric values

--- hw4/nn/__main__.py
+++ hw4/nn/__main__.py
@@ -9,28 +9,46 @@ from .utils import NeuralNetwork
 # TODO: allow epoch of multiple result-output pairs
 
 
+# Test values courtesy of:
+# https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+
+#test_net =
+
+
+
+
 
 
 # test_net = NeuralNetwork([8,3,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8])
 # print(test_net)
 # print()
 sig = lambda z: 1/(1+np.exp(-z))
-mini_net = NeuralNetwork([2,1],[1,0],[1])
-input = np.array([1,0])
-weights_1 = np.array([[0.4,0.6],[0.9,0.1]])
+input = np.array([1,1,0])
+mini_net = NeuralNetwork([3,2,1],input,[1])
+# for l in mini_net.layers:
+#     print(l.wts)
+weights_0 = np.array([[0.6,0.7,0.2],[0.2,0.9,0.4],[0.7,0.5,0.8]])
+weights_1 = np.array([[0.4,0.6,0.1],[0.9,0.1,0.3]])
 weights_2 = np.array([[0.45,0.55]])
-mini_net.layers[0].b = 1 #change bias from zero so we can test it
-mini_net.layers[0].set_weights_arr(weights_1)
-mini_net.layers[1].set_weights_arr(weights_2)
+mini_net.layers[0].set_bias(np.array([1,1,1]).reshape(-1,1)) #change bias from zero so we can test it
+mini_net.layers[0].set_weights_arr(weights_0)
+mini_net.layers[1].set_weights_arr(weights_1)
+mini_net.layers[2].set_weights_arr(weights_2)
 
+encoder_inputs = [[1,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0],[0,0,1,0,0,0,0,0],[0,0,0,1,0,0,0,0],
+        [0,0,0,0,1,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0] ]
+encoder_outputs = [[1,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0],[0,0,1,0,0,0,0,0],[0,0,0,1,0,0,0,0],
+        [0,0,0,0,1,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0] ]
 
 test_net = NeuralNetwork([8,3,8],[1,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0])
-for i in range(1000):
+for i in range(100):
     print('**ITERATION',i,"**")
-    test_net.backpropagate()
-    print("output:",test_net.feedforward())
-    print("goal:",test_net.y)
+    test_net.backpropagate(new_x=encoder_inputs,new_y=encoder_outputs)
+    print('propagation finished')
+    results = test_net.feedforward(new_x=encoder_inputs)
+    for r in results:
+        print(list(map(lambda n:np.round(n,3),r)),max(r))
     print()
     print()
-print(test_net.feedforward(new_input=[0,1,0,0,0,0,0,0]))
+#print(mini_net.feedforward(new_input=[0,1,0,0,0,0,0,0]))
 ## INTERESTING, THIS ONLY ALLOWS OUTPUTS BETWEEN 0 and 1...oh right, because of the sigmoid duh
--- hw4/nn/utils.py
+++ hw4/nn/utils.py
@@ -1,6 +1,8 @@
 # classes and general utility functions for implementing a neural network
 import numpy as np
 
+## SINGLE LAYER
+
 class Layer:
     """
     Represents a single layer of a neural network.
@@ -28,12 +30,12 @@ class Layer:
         self.f_name = act_fn_name if act_fn_name is not None else "sigmoid"
         #the bias value to use for each neuron in the layer. Use default 0 b/c:
         #https://datascience.stackexchange.com/questions/17987/how-should-the-bias-be-initialized-and-regularized
-        self.b = layer_bias if layer_bias is not None else 0
+        self.b = layer_bias if layer_bias is not None else np.zeros(shape=(n_nodes,1))
         #2D np array of weights, nodes(r) x inputs(c)
         #use xavier initialization because:
         # https://www.quora.com/What-are-good-initial-weights-in-a-neural-network
         # and https://stackoverflow.com/questions/48641192/xavier-and-he-normal-initialization-difference
-        arr = np.random.randn(n_inputs,n_nodes) * np.sqrt(1/n_inputs)
+        arr = np.random.randn(n_nodes,n_inputs) * np.sqrt(1/n_inputs)
         self.wts = arr
 
     # Overload the __repr__ operator to make printing simpler.
@@ -55,9 +57,13 @@ class Layer:
     def get_dim(self):
         return self.wts.shape
 
-    def set_bias(self,new_bias):
+    def set_bias(self,new_bias,change_dimensions_okay=False):
         """Replaces the layer's bias term with the input value"""
-        self.b=new_bias
+        if new_bias.shape == self.b.shape or change_dimensions_okay == True:
+            self.b = new_bias
+        else:
+            raise ValueError("Dimensions Mismatch with self.b disallowed: \
+                    %s self vs %s input" % (self.b.shape,new_bias.shape))
 
     def set_weights_arr(self,array,change_dimensions_okay=False):
         """Replaces the weights array with the given array. Does not allow
@@ -88,15 +94,15 @@ class Layer:
         Input: array-like of values, same length as the layer's number of cols
         Output: np.array of the activation values from each neuron, given the input
         """
-        neuron_outputs = np.matmul(self.wts,input_values) + self.b #term aka 'z'
-        return self.f(neuron_outputs) #apply AF to z
+        hyperlines = np.add(np.multiply(self.wts,input_values),self.b)
+        node_outputs = np.sum(hyperlines,axis=1)
+        return self.f(node_outputs) #apply AF to z
 
 
 
+## EXTERNAL POSSIBLY USEFUL FUNCTIONS
 
-
-
-def single_example_loss(y,true_y):
+def error(y,true_y):
     """Returns half the square of the Euclidean distance between y and true_y"""
     return ( np.abs(true_y-y)^2 ) * 0.5
 
@@ -116,8 +122,12 @@ def loss(nn,ys,true_ys,wd_param=0.001):
     return avg_sum_squares_err + weight_decay_term
 
 
+def array_sanitize(input):
+    """helper function to ensure inputs behave well for single and multiple test sets"""
+    return input if isinstance(input[0],(list,tuple,np.ndarray)) else np.array([input])
 
 
+## NEURAL NETWORK
 
 class NeuralNetwork:
     """
@@ -132,16 +142,18 @@ class NeuralNetwork:
     wd: the weight decay parameter
     """
     # TODO implement sparsity ^rho in cost function??
-    def __init__(self,layer_sizes,inputs_x,true_results_y,alpha=3,wd=0.0001):
+    def __init__(self,layer_sizes,inputs_x,true_results_y,alpha=0.5,wd=0.001):
         # make layers for every transition, including inputs->first layer
-        layer_dims = [(len(inputs_x),layer_sizes[0])] + [
-          (layer_sizes[i],layer_sizes[i+1]) for i in range(len(layer_sizes)-1) ]
+        layer_dims = [(layer_sizes[0],len(inputs_x))] + [
+          (layer_sizes[i+1],layer_sizes[i]) for i in range(len(layer_sizes)-1) ]
         self.layers = np.array([ Layer(n,i) for n,i in layer_dims ])
-        self.x = inputs_x
-        self.y = true_results_y
+        # housekeeping for good behavior when testing multiple sets of inputs
+        self.x = array_sanitize(inputs_x)
+        self.y = array_sanitize(true_results_y)
+        # set constants
         self.alpha = alpha
         self.wd = wd
-        self.m = len(self.layers)
+        self.dim = layer_sizes
 
     def __repr__(self):
         #much easier to read printing version
@@ -157,29 +169,67 @@ class NeuralNetwork:
     def get_blank_weights_array(self):
         return np.array([ np.zeros(l.wts.shape) for l in self.layers ])
 
-    def feedforward(self,new_input=None,return_activations=False):
+
+    def feedforward_single(self,input,return_activations=False):
+        """
+        implements feeding forward for a single set of inputs
+
+        Input: 1D array of input values, optional flag to return all activations
+        Output: 1D array of output values from final layer
+                    -OR-
+                Tuple of (array of outputs, array of activations for each layer)
+                NOTE: the first array of activations is the inputs!
+        """
+        if return_activations == True:
+            activation = np.array([np.zeros(n) for n in [len(input)]+self.dim])
+            activation[0] = input
+            for i,layer in enumerate(self.layers):
+                output = layer.feedforward_layer(input)
+                activation[i+1] = output
+                input = output
+            return output,activation
+        else:
+            for layer in self.layers:
+                output = layer.feedforward_layer(input)
+                input = output
+            return output
+
+
+    def feedforward(self,new_x=None,return_activations=False):
         """
         implements feeding forward through the whole network
 
         Input: optional array of inputs, optional flag to return all activations
                (if no inputs given, use self.x)
-        Output: Array of output values from the final layer
+        Output: Array of, for each set of inputs, the array of output values
+                from the final layer
                         -OR- (if return_activations set to True)
-                Tuple of (array of outputs, array of activations for each layer)
+                Tuple (outputs f.a. input sets, activations f.a. input sets)
+
+                NOTE: the first array of each activations is the inputs!
         """
         #allow but don't require passing in new inputs, copy so we don't alter
-        input = np.copy(new_input) if new_input is not None else np.copy(self.x)
+        input = array_sanitize(np.copy(new_x)) if new_x is not None else np.copy(self.x)
+        #initialize array of outputs
+        results = np.array([ np.zeros(self.dim[-1]) for m in input])
+
+        #make array of activation arrays, one for each layer + inputs
         if return_activations == True:
-            activations = np.array([np.zeros(l.wts.shape) for l in self.layers])
-            for i,layer in enumerate(self.layers):
-                output = layer.feedforward_layer(input)
-                activations[i] = output
-                input = output
-            return output,activations
+            activations = np.array([
+                np.array([np.zeros(n) for n in [len(input[0])]+self.dim])
+                                                               for m in input ])
+            for m,input_set in enumerate(input): # do this for every test
+                results[m],activations[m] = self.feedforward_single(input_set,return_activations=True)
+            print()
+            return results,activations
+
+        #OR just want output, don't need to store activations
+        # (for more efficient testing after training!)
         else:
-            for layer in self.layers:
-                input = layer.feedforward_layer(input)
-            return input
+            for m,input_set in enumerate(input):
+                results[m] = self.feedforward_single(input_set)
+            return results
+
 
     def backpropagate(self,new_y=None,new_x=None):
         """
@@ -191,52 +241,72 @@ class NeuralNetwork:
         Outputs: None (updates weights and biases in self.layers)
         """
         print("Beginning Backpropagation")
-        #allow but don't require passing in true output labels, copy for safety
-        true_y = np.copy(new_y) if new_y is not None else np.copy(self.y)
-        input = np.copy(new_x) if new_x is not None else np.copy(self.x)
-        output,activations = self.feedforward(input,return_activations=True)
+        #print([ (layer.wts.shape,layer.b.shape) for layer in self.layers])
+        #allow but don't require passing in true output labels, copy for safety, ensure 2D
+        true_ys = array_sanitize(np.copy(new_y)) if new_y is not None else np.copy(self.y)
+        inputs = array_sanitize(np.copy(new_x)) if new_x is not None else np.copy(self.x)
+        outputs,activations = self.feedforward(inputs,return_activations=True)
 
-        # initialize arrays to hold updated weights and biases
-        new_weights = np.array([ np.zeros(l.wts.shape) for l in self.layers ])
-        new_biases = np.zeros(self.m)
+        # initialize arrays to hold partial derivatives of weights and biases
+        grad_weights = [0]*len(self.layers)
+        grad_biases = [0]*len(self.layers)
+        #grad_weights= np.array([ np.zeros(l.wts.shape) for l in self.layers ])
+        #grad_biases = np.array([ np.zeros(l.b.shape) for l in self.layers ])
+        # initialize array to hold deltas for nodes in each layer, get outer delta
+        deltas = np.array([ np.zeros(len(l.wts)) for l in self.layers ])
 
-        # initialize array to hold deltas for each layer, computer outer delta
-        deltas = np.array([ np.zeros(l.wts.shape) for l in self.layers ])
-
-        # Fill output layer values
-        # NOTE: Hadamard product is numpy default vector * operation
-        # NOTE: for sigmoid, derivative s'(z) = s(z) * (1-s(z))
-        delta_outer = -(true_y-output) * ( output*(1-output) )
-        new_weights[-1] = delta_outer*np.transpose(activations[-1])
-        new_biases[-1] = delta_outer
-        deltas[-1] = delta_outer
-
-        # Iterate through all non-output layers in reverse, with their activations
-        for i in range(self.m-2,-1,-1):
-            l = self.layers[i]
-            a = activations[i]
-            ## TODO: There must be a better indexing strategy than this...
-            # calculate delta from the delta of the next layer
-            delta = ( np.transpose(l.wts)*deltas[i+1] ) * ( a*(1-a) )
-            deltas[i] = delta
-            # calculate layer's errors from the delta of the next layer
-            new_weights[i] = deltas[i+1]*np.transpose(a)
-            new_biases[i] = deltas[i+1]
+        # accumulate partial derivatives for every input set
+        for output,activation,true_y in zip(outputs,activations,true_ys):
+            # Fill output layer values
+            # NOTE: Hadamard product is np.multiply(A,B)
+            # NOTE: for sigmoid, derivative s'(z) = s(z)*(1-s(z)), s(z)_i = output_i
+            delta_outer = np.multiply(-(true_y-output),( output*(1-output) ))
+            grad_weights[-1] = np.array([activation[-2]])*np.array([delta_outer]).T #cast 1D as 2D to ensure proper multiplication
+            grad_biases[-1] = delta_outer.reshape(-1,1)
+            deltas[-1] = delta_outer
+            # print(2)
+            # print('WANT: delta, grad wts, grad bias',1,(1,2),1)
+            # print('layer',self.layers[-1].wts)
+            # print('activation',activation[-1])
+            # print('delta',delta_outer)
+            # print('gw',grad_weights[-1])
+            # print('gb',grad_biases[-1])
+            # Iterate through all non-output layers in reverse, with their activations
+            for i in range(len(self.layers)-2,-1,-1):
+                #print(i)
+                # if i == 1:
+                #     print("WANT: delta, grad wts, grad bias",2,(2,3),2)
+                # else:
+                #     print("WANT:, delta, grad wts, grad bias",3,(3,3),3)
+                l,next_l = self.layers[i],self.layers[i+1]
+                a,prev_a = activation[i+1],activation[i] #activations[0] is inputs
+                # print('next_l',next_l.wts.shape)
+                # print('layer',l.wts.shape)
+                # print('activation',a,a.shape)
+                # print('prev_activation',prev_a,prev_a.shape)
+                #print('next_delta',deltas[i+1].shape)
+                # calculate delta from the delta of the next layer
+                delta = np.multiply( np.matmul(next_l.wts.T,deltas[i+1]), (a*(1-a)) )
+                deltas[i] = delta
+                # print('this delta',delta,delta.shape)
+                # calculate partial derivatives for weights and bias in the layer
+                # TODO: below. WHY IS MATRIX MULTIPLICATION ORDER REVERSED?? It makes a 3x2 the other way, and we want a 2x3
+                grad_weights[i] += np.array([prev_a])*np.array([delta]).T #cast both 1Ds as 2D to ensure proper matrix multiplication
+                grad_biases[i] += delta.reshape(-1,1) #bias maintained in col form
+                # print('grad_wts',grad_weights[i])
+                # print('grad_biases',grad_biases[i])
+                # print()
 
         # Iterate through all layers forward, updating weights
         for i,layer in enumerate(self.layers):
-            #print("   Layer",i,":")
+            # print()
+            # print('prev wts',l.wts)
+            # print('new wts',grad_weights[i])
+            # print('prev b',l.b)
+            # print('new b',grad_biases[i])
             weight_decay_term = self.wd*layer.wts
-            error_term = (1/self.m) * new_weights[i]
+            error_term = (1/len(inputs)) * grad_weights[i]
             updated_wts = layer.wts - self.alpha*(error_term+weight_decay_term)
-            updated_bias = layer.b - self.alpha*(1/self.m)*new_biases[i]
-            #print("Delta:",deltas[i])
-            #print("Partial Deriv:",new_weights[i])
-            #print("Updated Weights:",updated_wts)
+            updated_bias = layer.b - self.alpha*(1/len(inputs))*grad_biases[i]
             layer.set_weights_arr(updated_wts)
             layer.set_bias(updated_bias)
-
-        #print("Deltas",deltas)
-        #print("Partial Derivs",new_weights)
-        #print("Partial Bias Derivs",new_biases)
-        #print("New Weights",[ [np.round(r,3) for r in l.wts] for l in self.layers ])
